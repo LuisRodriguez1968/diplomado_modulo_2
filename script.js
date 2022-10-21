8accf19a6167ed18ccf7df3e6ddfca5c1af72ec2
LuisRodriguez1968 <luchobetorh@gmail.com>
2022-10-20 20:42:56 -0500

    nujevo dato

---
+++ script.js
@@ -0,0 +1,9 @@
+function primera(param1){
+    console.log(typeof param1)
+    if (typeof param1 == 'number'){
+        console.log(`My number: ${param1}`)
+    }
+    else {
+        console.log("Not number")
+    }
+}
