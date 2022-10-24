0ffe27a1c39181cfa37000906a5267e2710fa862
LuisRodriguez1968 <luchobetorh@gmail.com>
2022-10-24 11:19:31 -0500

    holamundo

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
-function primera(param1){
-    console.log(typeof param1)
-    if (typeof param1 == 'number'){
-        console.log(`My number: ${param1}`)
-    }
-    else {
-        console.log("Not number")
+// function primera(param1){
+//     console.log(typeof param1)
+//     if (typeof param1 == 'number'){
+//         console.log(`My number: ${param1}`)
+//     }
+//     else {
+//         console.log("Not number")
+//     }
+// }
+function primera(param1, param2) {
+    let array1 = [param1][param1];
+    array1.fill(param2, 0, param1)
+    for (let i = 0; i < param1; i++) {
+            console.log(array2)
     }
 }
