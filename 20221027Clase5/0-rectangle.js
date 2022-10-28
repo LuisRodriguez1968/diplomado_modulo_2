d06a154a85da60005525a2f9685d576ee888bc76
LuisRodriguez1968 <luchobetorh@gmail.com>
2022-10-27 21:28:34 -0500

    Class

---
+++ 20221027Clase5/0-rectangle.js
@@ -0,0 +1,15 @@
+// class Persona{
+//     constructor(numeroId, nombre){
+//         this.numeroId=documento
+//         this.nombre=nombres
+//     }
+// }
+// const persona1 =new Persona(documento=12121,nombres="Luis Alberto")
+// const persona2 =new Persona(44512121)
+
+// console.log(persona1,persona2)
+
+class Rectangle{
+    constructor(){}
+}
+module.exports = class Rectangle {}
