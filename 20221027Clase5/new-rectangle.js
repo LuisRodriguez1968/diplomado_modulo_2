f47a6b227bbc34ae85158e381aece49f34d9a122
LuisRodriguez1968 <luchobetorh@gmail.com>
2022-11-03 22:12:50 -0500

    cards
    cards

---
+++ 20221027Clase5/new-rectangle.js
@@ -0,0 +1,17 @@
+// class Persona{
+//     constructor(numeroId, nombre){
+//         this.numeroId=documento
+//         this.nombre=nombres
+//     }
+// }
+// const persona1 =new Persona(documento=12121,nombres="Luis Alberto")
+// const persona2 =new Persona(44512121)
+
+// console.log(persona1,persona2)
+
+module.exports = class Rectangle {
+    constructor(h, w) {
+        this.width = w
+        this.height = h
+    }
+}
---
+++ 20221101Cards/app.js
@@ -0,0 +1,31 @@
+// const lista = document.getElementById('lista')
+// const arrayItem = ['primer','segundo','tercero']
+// const fragment = document.createDocumentFragment()
+// arrayItem.forEach(item=>{
+// const li = document.createElement('li')
+// li.textContent = item
+// fragment.appendChild(li)
+// console.log(fragment)
+// })
+// lista.appendChild(fragment)
+
+const cards=document.getElementById('cards')
+console.log(cards)
+const div_body=document.createElement("div")
+const titulo=document.createElement("h5")
+titulo.textContent="Titulo de la tarjeta"
+const parrafo=document.createElement("p")
+parrafo.textContent="este es texto para el parrafo"
+const boton=document.createElement("b")
+boton.textContent="este es boton"
+const imagen=document.createElement("img")
+imagen.setAttribute('src','...')
+div_body.setAttribute('class',"card-body")
+titulo.setAttribute('class','card-title text-center')
+parrafo.setAttribute('class','card-text p-4')
+boton.setAttribute('class',"btn btn-primary")
+cards.appendChild(imagen)
+cards.appendChild(div_body)
+div_body.appendChild(titulo)
+div_body.appendChild(parrafo)
+div_body.appendChild(boton)
