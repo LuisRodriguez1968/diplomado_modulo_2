92bec0153ac687bd61a6d4b4b3229b5316edddec
LuisRodriguez1968 <luchobetorh@gmail.com>
2022-11-04 11:10:31 -0500

    aa

--- 20221104Cards/app.js
+++ 20221104Cards/app.js
@@ -1,21 +1,22 @@
+const 
 
+const cards = document.getElementById('cards')
+const imagen = document.createElement("img")
+const div_body = document.createElement("div")
+const titulo = document.createElement("h5")
+const parrafo = document.createElement("p")
+const boton = document.createElement("b")
 
+titulo.textContent = "Titulo de la tarjeta"
+parrafo.textContent = "este es texto para el parrafo"
+boton.textContent = "Pulse aqui"
 
-const cards=document.getElementById('cards')
-console.log(cards)
-const div_body=document.createElement("div")
-const titulo=document.createElement("h5")
-titulo.textContent="Titulo de la tarjeta"
-const parrafo=document.createElement("p")
-parrafo.textContent="este es texto para el parrafo"
-const boton=document.createElement("b")
-boton.textContent="este es boton"
-const imagen=document.createElement("img")
-imagen.setAttribute('src','...')
-div_body.setAttribute('class',"card-body")
-titulo.setAttribute('class','card-title text-center')
-parrafo.setAttribute('class','card-text p-4')
-boton.setAttribute('class',"btn btn-primary")
+imagen.setAttribute('src', 'img_avatar3.png')
+div_body.setAttribute('class', "card-body text-center")
+titulo.setAttribute('class', 'card-title text-center')
+parrafo.setAttribute('class', 'card-text p-4')
+boton.setAttribute('class', "btn btn-primary")
+
 cards.appendChild(imagen)
 cards.appendChild(div_body)
 div_body.appendChild(titulo)
