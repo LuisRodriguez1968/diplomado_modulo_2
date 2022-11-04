e818605f1a19a87aad5ad33967d3373ede1ec320
LuisRodriguez1968 <luchobetorh@gmail.com>
2022-11-04 14:26:28 -0500

    Update app.js

--- 20221104Cards/app.js
+++ 20221104Cards/app.js
@@ -1,24 +1,36 @@
-const 
+// let i=0;
+// while(i<5){
+let text = "";
+const data[{titulo = "titulo1",
+            parrafo = "parrafo1" }
+        ];
 
-const cards = document.getElementById('cards')
-const imagen = document.createElement("img")
-const div_body = document.createElement("div")
-const titulo = document.createElement("h5")
-const parrafo = document.createElement("p")
-const boton = document.createElement("b")
+data.forEach(myFunction);
 
-titulo.textContent = "Titulo de la tarjeta"
-parrafo.textContent = "este es texto para el parrafo"
-boton.textContent = "Pulse aqui"
+const cards = document.getElementById('cards').innerHTML=text;
 
-imagen.setAttribute('src', 'img_avatar3.png')
-div_body.setAttribute('class', "card-body text-center")
-titulo.setAttribute('class', 'card-title text-center')
-parrafo.setAttribute('class', 'card-text p-4')
-boton.setAttribute('class', "btn btn-primary")
-
-cards.appendChild(imagen)
-cards.appendChild(div_body)
-div_body.appendChild(titulo)
-div_body.appendChild(parrafo)
-div_body.appendChild(boton)
+function myFunction(repite){
+    const imagen = document.createElement("img")
+    const div_body = document.createElement("div")
+    const titulo = document.createElement("h5")
+    const parrafo = document.createElement("p")
+    const boton = document.createElement("b")
+    
+    titulo.textContent = "Titulo"
+    parrafo.textContent = "este es texto para el parrafo"
+    boton.textContent = "Pulse"
+    
+    imagen.setAttribute('src', 'img_avatar3.png')
+    div_body.setAttribute('class', "card-body text-center")
+    titulo.setAttribute('class', 'card-title text-center')
+    parrafo.setAttribute('class', 'card-text p-4')
+    boton.setAttribute('class', "btn btn-primary")
+    
+    cards.appendChild(imagen)
+    cards.appendChild(div_body)
+    div_body.appendChild(titulo)
+    div_body.appendChild(parrafo)
+    div_body.appendChild(boton)
+  // text+=repite;
+}
+//i++ }
