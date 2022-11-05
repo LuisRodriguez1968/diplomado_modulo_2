5cdbe853d08f6cb020ea33d512cc566c503f5e8c
LuisRodriguez1968 <luchobetorh@gmail.com>
2022-11-04 20:29:16 -0500

    cards

--- 20221104Cards/app.js
+++ 20221104Cards/app.js
@@ -1,36 +1,27 @@
-// let i=0;
-// while(i<5){
-let text = "";
-const data[{titulo = "titulo1",
-            parrafo = "parrafo1" }
-        ];
-
-data.forEach(myFunction);
-
-const cards = document.getElementById('cards').innerHTML=text;
-
-function myFunction(repite){
-    const imagen = document.createElement("img")
-    const div_body = document.createElement("div")
-    const titulo = document.createElement("h5")
-    const parrafo = document.createElement("p")
-    const boton = document.createElement("b")
-    
-    titulo.textContent = "Titulo"
-    parrafo.textContent = "este es texto para el parrafo"
-    boton.textContent = "Pulse"
-    
-    imagen.setAttribute('src', 'img_avatar3.png')
-    div_body.setAttribute('class', "card-body text-center")
-    titulo.setAttribute('class', 'card-title text-center')
-    parrafo.setAttribute('class', 'card-text p-4')
-    boton.setAttribute('class', "btn btn-primary")
-    
-    cards.appendChild(imagen)
-    cards.appendChild(div_body)
-    div_body.appendChild(titulo)
-    div_body.appendChild(parrafo)
-    div_body.appendChild(boton)
-  // text+=repite;
+function fcard(value) {
+   let i = 0
+   while (i < value) {
+      const cards = document.getElementById('cards')
+      console.log(cards)
+      const div_body = document.createElement("div")
+      const titulo = document.createElement("h5")
+      titulo.textContent = "Titulo de la tarjeta"
+      const parrafo = document.createElement("p")
+      parrafo.textContent = "este es texto para el parrafo"
+      const boton = document.createElement("b")
+      boton.textContent = "este es boton"
+      const imagen = document.createElement("img")
+      imagen.setAttribute('src', 'cards.jpg', 'class', 'card-img-top')
+      div_body.setAttribute('class', "card-body")
+      titulo.setAttribute('class', 'card-title text-center')
+      parrafo.setAttribute('class', 'card-text p-4')
+      boton.setAttribute('class', "btn btn-primary")
+      cards.appendChild(imagen)
+      cards.appendChild(div_body)
+      div_body.appendChild(titulo)
+      div_body.appendChild(parrafo)
+      div_body.appendChild(boton)
+      i++
+   }
 }
-//i++ }
+fcard(5)
