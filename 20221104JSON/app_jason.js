df419766deac226e0175217afcca7cbae4cc64b1
LuisRodriguez1968 <luchobetorh@gmail.com>
2022-11-04 22:32:18 -0500

    otro

---
+++ 20221104JSON/app_jason.js
@@ -0,0 +1,17 @@
+const fs =require("fs");
+const path =require("path");
+
+const pathJSON= path.join(__dirname,"./db.json");
+
+const readJSON = () =>{
+    const data = fs.readFileSync(pathJSON,"utf-8");
+    return JSON.parse(data);
+};
+
+const writeJSON = (data) => {
+    fs.writeFileSync(pathJSON,data);
+}
+
+const data = readJSON("");
+
+console.log(data);
