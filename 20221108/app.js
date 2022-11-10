dc70e51cde5c14e13281b868f9579766540a085e
LuisRodriguez1968 <luchobetorh@gmail.com>
2022-11-10 14:29:06 -0500

    20221108

--- 20221108/app.js
+++ 20221108/app.js
@@ -1,33 +1,68 @@
+let nCards =
+[
+   {
+       "title": "Card 1",
+       "content": "Este es el texto que debe observarse en el card-text",
+       "buton": "Pulsame"
+   },
+   {
+       "title": "Card 2",
+       "content": "Este es el texto que debe observarse en el card-text",
+       "buton": "Pulsame"
+   },
+   {
+       "title": "Card 3",
+       "content": "Este es el texto que debe observarse en el card-text",
+       "buton": "Pulsame"
+   },
+   {
+       "title": "Card 4",
+       "content": "Este es el texto que debe observarse en el card-text",
+       "buton": "Pulsame"
+   },
+   {
+       "title": "Card 5",
+       "content": "Este es el texto que debe observarse en el card-text",
+       "buton": "Pulsame"
+   },
+   {
+       "title": "Card 6",
+       "content": "Este es el texto que debe observarse en el card-text",
+       "buton": "Pulsame"
+   }
+  ]
 
+   nCards.forEach(elemento => createCard(elemento.Card));
 
+   const cards=document.getElementById('cards')
+   cards.setAttribute('class',"card")
+ 
+   const createCard = (elemento) => {
+      const fragment = document.createDocumentFragment();
 
-
-
-
- function fcard(value){
-    let i=0
-    while(i<value){ 
-           const cards=document.getElementById('cards')
-    console.log(cards)
-    const div_body=document.createElement("div")
-    const titulo=document.createElement("h5")
-    titulo.textContent="Titulo de la tarjeta"
-    const parrafo=document.createElement("p")
-    parrafo.textContent="este es texto para el parrafo"
-    const boton=document.createElement("b")
-    boton.textContent="este es boton"
-    const imagen=document.createElement("img")
-    imagen.setAttribute('src','cards.jpg','class','card-img-top')
-    div_body.setAttribute('class',"card-body")
-    titulo.setAttribute('class','card-title text-center')
-    parrafo.setAttribute('class','card-text p-4')
-    boton.setAttribute('class',"btn btn-primary")
-    cards.appendChild(imagen)
-    cards.appendChild(div_body)
-    div_body.appendChild(titulo)
-    div_body.appendChild(parrafo)
-    div_body.appendChild(boton)
-    i++}
-    
- }
- fcard(5)
+      const ppal=document.createElement("div")
+      const cuerpo=document.createElement("div")
+      const titulo=document.createElement("h5")
+      titulo.textContent="Titulo de la tarjeta"
+      const parrafo=document.createElement("p")
+      parrafo.textContent="este es texto para el parrafo"
+      const boton=document.createElement("b")
+      boton.textContent="este es boton"
+      const imagen=document.createElement("img")
+       
+      ppal.setAttribute('class',"card")
+      cuerpo.setAttribute('class',"card-body")
+      imagen.setAttribute('src','cards.jpg','class','card-img-top')
+      titulo.setAttribute('class','card-title text-center')
+      parrafo.setAttribute('class','card-text p-4')
+      boton.setAttribute('class',"btn btn-primary")
+      
+      ppal.appendChild(imagen)
+      ppal.appendChild(cuerpo)
+      cuerpo.appendChild(titulo)
+      cuerpo.appendChild(parrafo)
+      cuerpo.appendChild(boton)
+      fragment.appendChild(ppal)
+      cards.appendChild(fragment)
+   }
+   createCard();
